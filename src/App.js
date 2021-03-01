e0e0ad7172394fdd89a58e5ad31ded4ac69e1352
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2021-03-01 08:49:39 -0300

    Router and constant

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+const Login = lazy(() => import('./pages/Login'));
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path='/login' component={Login} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
+}
