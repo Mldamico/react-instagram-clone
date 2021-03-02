bfa35090536ca4dea1abab25824a78c1ac4cbc8d
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2021-03-02 08:56:47 -0300

    Tailwind config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
 const Login = lazy(() => import('./pages/Login'));
 
 export default function App() {
@@ -7,7 +8,7 @@ export default function App() {
     <Router>
       <Suspense fallback={<p>Loading...</p>}>
         <Switch>
-          <Route path='/login' component={Login} />
+          <Route path={ROUTES.LOGIN} component={Login} />
         </Switch>
       </Suspense>
     </Router>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,3 +1,23 @@
-export const Login = () => {
-  return <div>Login PPAGE</div>;
+import { useContext, useState, useEffect } from 'react';
+import { useHistory } from 'react-router';
+import FirebaseContext from '../context/firebase';
+const Login = () => {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
+  const isInvalid = password === '' || email === '';
+
+  const handleLogin = () => {};
+  useEffect(() => {
+    document.title = 'Login - Instagram';
+  }, []);
+  return (
+    <div className='container flex mx-auto max-w-screen-md items-center h-screen'>
+      <p>PROBANDO</p>
+    </div>
+  );
 };
+
+export default Login;
